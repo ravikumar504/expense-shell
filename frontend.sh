1baa24b6b3e25e539ea497b2ef1b6ae0467705fd
Ravikumar <pandu.ravi218@gmail.com>
2025-04-28 11:44:04 +0530

    expense-shell

---
+++ frontend.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+validate() 
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2.... failure"
+        exit 1
+    else 
+        echo "$2.... success"
+    fi
+}
+
+if [ $userid -ne 0 ]
+then 
+    echo "you must need root access to execute this script"
+    exit 1
+fi
+
+dnf install nginx -y 
+validate $? "installing nginx"
+
+systemctl enable nginx
+validate $? "enable nginx"
+
+systemctl start nginx
+validate $? "start nginx"
+
+rm -rf /usr/share/nginx/html/*
+validate $? 'removing code'
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+validate $? "downloading code"
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+validate $? "unzip code"
+
+systemctl restart nginx
+validate $? "restart nginx"
